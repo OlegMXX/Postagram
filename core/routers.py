3d0687295f3120dfad8709ed50679c8bf8433470
OllyYa <exrhodo@gmail.com>
2023-10-23 14:33:19 +0300

    login process with jwt

--- core/routers.py
+++ core/routers.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 
-from core.auth.viewsets import RegisterViewSet
+from core.auth.viewsets import RegisterViewSet, LoginViewSet
 from core.user.viewsets import UserViewSet
 
 router = routers.SimpleRouter()
@@ -8,6 +8,8 @@ router = routers.SimpleRouter()
 
 # AUTH
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+router.register(r'auth/login', LoginViewSet, basename='auth-login')
+
 # USER
 router.register(r'user', UserViewSet, basename='user')
 
